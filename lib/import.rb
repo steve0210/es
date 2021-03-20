47ef3d42769966f9a749a2af7c9de7bf61ef2aea
Steve Meadows <smeadows@sonichealthcareusa.com>
2021-03-20 03:11:25 -0500

    modify import class

--- lib/import.rb
+++ lib/import.rb
@@ -1,12 +1,46 @@
 
 class Import
+  ENV = {
+    "kibana_system" => ["ELASTICSEARCH_PASSWORD"]
+  }
 
-  def initialize(in_file)
+  def initialize(in_file, env_file)
+    process_in(in_file)
+    process_env(env_file)
+    replace_env(env_file)
+  end
+
+  def process_in(in_file)
     in_file.each do |line|
       if m = line.match(/PASSWORD (.*) = (.*)/)
-        passwords[m[1]] = m[2]
+        passwords[m[1]] = m[2].strip
       end
     end
+  end
+
+  def process_env(env_file)
+    File.readlines(env_file).map(&:strip).each do |line|
+      lines << (match(line) || line)
+    end
+  end
+
+  def replace_env(env_file)
+    File.write(env_file, lines.join("\n") + "\n")
+  end
+
+  def match(line)
+    passwords.each do |key, value|
+      (envs = ENV[key]) && envs.each do |env|
+        if m = line.match(/#{env}=/)
+          return "#{env}=#{value}"
+        end
+      end
+    end
+    nil
+  end
+
+  def lines
+    @lines ||= []
   end
 
   def passwords
--- scripts/set_users.sh
+++ scripts/set_users.sh
@@ -4,7 +4,9 @@ $LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
 require 'import'
 require 'client'
 
-import = Import.new(ARGF)
+import = Import.new(ARGF, File.expand_path("../../.env", __FILE__))
+
+puts import.lines.inspect
 
 client = Client.new(import.passwords["elastic"]).client
 response = client.cluster.health
