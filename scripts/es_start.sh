483fe12fa32f3f69a8b5ebdbd3e42919e01dc9f7
Steve Meadows <smeadows@sonichealthcareusa.com>
2021-03-20 00:24:05 -0500

    modify start script

--- scripts/es_start.sh
+++ scripts/es_start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker-compose up -d elasticsearch && ./scripts/elasticsearch-setup-passwords.sh
+docker-compose up -d elasticsearch && \
+  ./scripts/elasticsearch-setup-passwords.sh | docker-compose run --rm -T users su - rails -c /var/www/es/current/scripts/set_users.sh
