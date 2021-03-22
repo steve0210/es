211d590b55cd46d3a9df0720b23bc925fe580a4b
Steve Meadows <smeadows@sonichealthcareusa.com>
2021-03-22 03:03:04 -0500

    add reader user

--- scripts/set_users.sh
+++ scripts/set_users.sh
@@ -5,7 +5,7 @@ $LOAD_PATH.unshift(File.expand_path(File.join(path, "lib"), __FILE__))
 require 'import'
 require 'client'
 
-import = Import.new(ARGF, File.expand_path(File.join(path, ".env"), __FILE__))
+import = Import.new(ARGF, File.expand_path(File.join(path, "kibana", ".env"), __FILE__))
 
 client = Client.new(
   File.expand_path(File.join(path, "config"), __FILE__),
