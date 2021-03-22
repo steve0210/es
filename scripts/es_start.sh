1b03e034f3cf104cc691bf7e303795f004eca82c
Steve Meadows <smeadows@sonichealthcareusa.com>
2021-03-21 23:25:48 -0500

    add metricbeat

--- scripts/es_start.sh
+++ scripts/es_start.sh
@@ -4,4 +4,5 @@ docker-compose up -d elasticsearch && \
   sleep 30 && \
   yes | \
   docker-compose exec -T elasticsearch /usr/share/elasticsearch/bin/elasticsearch-setup-passwords auto | \
-  docker-compose run --rm -T users su - rails -c /var/www/es/current/scripts/set_users.sh | tee ./.env
+  docker-compose run --rm -T users su - rails -c /var/www/es/current/scripts/set_users.sh | \
+  tee ./kibana/.env
