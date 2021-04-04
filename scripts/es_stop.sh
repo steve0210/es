32058730c004ff5408354174c316f49d644094bb
Steve Meadows <smeadows@sonichealthcareusa.com>
2021-04-04 00:52:30 -0500

    add stop script

--- scripts/es_stop.sh
+++ scripts/es_stop.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-(cd metricbeat/ && docker-compose down) && \
-  (cd kibana/ && docker-compose down) && \
+(cd ./dotnet/TodoApi && docker-compose down) && \
+(cd ./apm-server && docker-compose down) && \
+(cd ./metricbeat && docker-compose down) && \
+  (cd ./kibana && docker-compose down) && \
   docker-compose down
